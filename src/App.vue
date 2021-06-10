d557898a5e699a83a251ce9f3b5f9d0e3df4a6eb
Vlad Basoon <vbasoon@gmail.com>
2021-06-10 21:40:14 +0300

    data return

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :class="{ active: $route.name === 'Home' }" to="/"
+      >Home</router-link
+    >
+    |
+    <router-link :class="{ active: $route.name === 'Cart' }" to="/cart"
+      >Cart</router-link
+    >
   </div>
   <router-view />
 </template>
@@ -15,16 +20,27 @@
   color: #2c3e50;
 }
 
+.text-center {
+  text-align: center;
+  color: #2c3e50;
+}
+
 #nav {
   padding: 10px;
   width: 100%;
+  height: 30px;
+  background-color: whitesmoke;
+  line-height: 30px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: darkgrey;
+    text-decoration: none;
+    margin: 0 5px 0 5px;
+    font-size: 1.25rem;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &.active {
+      color: #2c3e50;
     }
   }
 }
@@ -33,5 +49,10 @@ html,
 body {
   padding: 0;
   margin: 0;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: rgb(245, 245, 245);
 }
 </style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="cart">
+    <h1>This is a Cart Page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
